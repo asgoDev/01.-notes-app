c02bab0c2bb07de926495a4daae6b97ce19e78f0
asgoDev <asgodev@gmail.com>
2024-02-14 00:49:18 -0400

    first try with PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+  manifest: {
+    name: "Notelicious",
+    short_name: "Notelicious",
+    description: "The perfect note list for you!",
+    theme_color: "#ffffff",
+    backgroun_color: "#ffffff",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    // orientation: 'portrait',
+    icons: [
+      {
+        src: './public/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: './public/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: './public/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any'
+      },
+      {
+        src: './public/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
+      }
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://asgoDev.github.io/Notelicious'
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+  base: "https://asgoDev.github.io/Notelicious",
+});
